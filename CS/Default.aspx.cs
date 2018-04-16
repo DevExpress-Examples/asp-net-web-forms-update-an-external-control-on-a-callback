b1bfeec2b6a6684e44efcbb143c350735d21a7e0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DevExpress.Web;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        ASPxGridView1.JSProperties["cpIsUpdated"] = "";
+    }
+    protected void ASPxGridView1_RowUpdated(object sender, DevExpress.Web.Data.ASPxDataUpdatedEventArgs e) {
+        if (e.Exception == null) {
+            ((ASPxGridView)sender).JSProperties["cpIsUpdated"] = e.Keys[0];
+        }
+    }
+}

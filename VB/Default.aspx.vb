1194a1ea2c54427731cc5324709a1a4b08f39927
mmakarova <makarova.marina@corp.devexpress.com>
2022-08-08 19:22:09 +0400

    Fix 15.1.10 VB

---
+++ VB/Default.aspx.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports DevExpress.Web
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxGridView1.JSProperties("cpIsUpdated") = ""
+	End Sub
+	Protected Sub ASPxGridView1_RowUpdated(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatedEventArgs)
+		If e.Exception Is Nothing Then
+			DirectCast(sender, ASPxGridView).JSProperties("cpIsUpdated") = e.Keys(0)
+		End If
+	End Sub
+End Class
